08a8b863e96e44943b4fd3bd87161e2c6e6cce3d
Neelaiah145 <rusumneelaiah@gmail.com>
2025-12-11 15:41:07 +0530

    login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import User
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email',"")
+        password = request.POST.get('password',"")
+        try:
+            user = User.objects.get(email = email,password = password)
+            request.session["user_id"] = user.id
+            return redirect('dashboard')
+        except User.DoesNotExist:
+            messages.error(request,"invalid or credits")
+            return (request,'login')
     
+            
     return render(request,'login.html')
 
 
