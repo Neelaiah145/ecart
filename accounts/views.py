18705371230140fb4d0fd37ff922bdf54b328920
Neelaiah145 <rusumneelaiah@gmail.com>
2025-12-11 15:58:09 +0530

    dashboard chnages

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User
-# Create your views here.
+
 def login(request):
     if request.method == 'POST':
-        email = request.POST.get('email',"")
-        password = request.POST.get('password',"")
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
         try:
-            user = User.objects.get(email = email,password = password)
+            user = User.objects.get(email=email, password=password)
             request.session["user_id"] = user.id
             return redirect('dashboard')
         except User.DoesNotExist:
-            messages.error(request,"invalid or credits")
-            return (request,'login')
-    
-            
-    return render(request,'login.html')
+            messages.error(request, "Invalid email or password")
+            return render(request, 'login.html', {'email': '', 'password': ''})
 
+    return render(request, 'login.html', {'email': '', 'password': ''})
 
 
 def dashboard(request):
-    return render(request,'dashboard.html')
+    if "user_id" not in request.session:
+        return redirect("login")
+    user = User.objects.get(id=request.session["user_id"])
+    return render(request, "dashboard.html", {"user": user})
